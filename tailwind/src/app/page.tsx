a0c3fc591f593176128cc615f1713318c03069d5
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-09-03 19:38:02 +0900

    트에일위인드

---
+++ tailwind/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" bg-red-500 h-screen w-full flex justify-center items-center  gap-10">
+      <div className=" bg-red-700 w-[100px] h-80  p-10 text-8xl text-blue-400 font-bold border-4  border-black shadow-2xl opacity-50">
+        집
+      </div>
+      <div className=" font-noto bg-red-800 w-80 h-80  text-center text-5xl  text-blue-500 leading-10 underline  rounded-full cursor-pointer">
+        가고
+      </div>
+      <div className=" bg-red-950 w-80 h-80   text-9xl text-blue-900  rounded-t-full border-4  border-black hover:bg-red-400">
+        싶다
+      </div>
+    </div>
+  );
+}
